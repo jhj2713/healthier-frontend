669930d234221b5d327165ffc76602833e7b5977
juhyojeong <juhyojeong58@gmail.com>
2022-06-29 11:58:20 +0900

    chore: style 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./lib/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import theme from "./lib/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
