199e8e6690a2c67f6cf76a1c2ff430f7cf9c3f4c
juhyojeong <juhyojeong58@gmail.com>
2022-06-29 11:35:11 +0900

    chore: globalstyle 생성

---
+++ src/lib/theme.ts
@@ -0,0 +1,32 @@
+const color = {
+  blue: "#587cf7",
+  green: "#d2fa64",
+  red: "#ff6c45",
+  grey_100: "#fdfdfd",
+  grey_200: "#e8e8e8",
+  grey_300: "#c5c8ce",
+  grey_400: "#999fa4",
+  grey_500: "#787a83",
+  grey_600: "#3f414f",
+  grey_650: "#353642",
+  grey_700: "#2e2f3a",
+  grey_750: "#23242e",
+  grey_800: "#1e1f27",
+  grey_850: "#17181c",
+  grey_900: "#131416",
+};
+
+const font_size = {
+  heading_1: 2.8,
+  heading_2: 2.4,
+  heading_3: 2.2,
+  heading_4: 2.0,
+  heading_5: 1.8,
+  body_1: 1.6,
+  body_2: 1.5,
+  body_3: 1.4,
+  body_4: 1.3,
+  description: 1.2,
+};
+
+export default { color, font_size };
