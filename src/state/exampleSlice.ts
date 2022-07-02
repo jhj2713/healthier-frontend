d966530bd05586e1069f4b728e5c326ebe9ffef5
asiloveyou <75231844+asiloveyou@users.noreply.github.com>
2022-07-02 15:30:09 -0400

    feat: 초기환경 설정

---
+++ src/state/exampleSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+
+// Define a type for the slice state
+interface ExampleState {
+  value: number;
+}
+
+// Define the initial state using that type
+const initialState: ExampleState = {
+  value: 0,
+};
+
+export const exampleSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = exampleSlice.actions;
+export const selectCount = (state: RootState) => state.example.value;
+
+export default exampleSlice.reducer;
