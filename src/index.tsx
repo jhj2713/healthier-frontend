a1df7348768592063ebe522cc5e1f24d89c41fef
asiloveyou <75231844+asiloveyou@users.noreply.github.com>
2022-07-02 21:41:28 -0400

    fix: react-redux 패키지 오류 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,19 @@ import GlobalStyle from "./lib/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./lib/theme";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./state/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
