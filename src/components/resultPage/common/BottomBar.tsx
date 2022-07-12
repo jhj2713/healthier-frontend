a070591d2f1f3bffa216187bd5c32ee8ac206877
juhyojeong <juhyojeong58@gmail.com>
2022-07-12 14:11:33 +0900

    feat: api 호출위치 정의

--- src/components/resultPage/common/BottomBar.tsx
+++ src/components/resultPage/common/BottomBar.tsx
@@ -30,12 +30,20 @@ const BottomButton = styled.section`
 `;
 
 const BottomBar = ({ curIndex, totalCount, setModal }: IBottomBar) => {
+  const handleSave = () => {
+    // 로그인되지 않은 경우
+    setModal(true);
+
+    // 로그인되어있는 경우
+    // 저장 api 호출
+  };
+
   return (
     <BottomContainer>
       {curIndex !== totalCount ? (
         <BottomNumber curIndex={curIndex} totalCount={totalCount} />
       ) : (
-        <BottomButton onClick={() => setModal(true)}>
+        <BottomButton onClick={handleSave}>
           <RoundButton
             outline="none"
             backgroundColor={theme.color.blue}
