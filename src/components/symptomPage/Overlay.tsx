ff6fe7fa6bda3c11a4534290e11729d28f02cb64
asiloveyou <75231844+asiloveyou@users.noreply.github.com>
2022-07-26 22:50:35 -0400

    chore: 오버레이 화면 높이 조정

--- src/components/symptomPage/Overlay.tsx
+++ src/components/symptomPage/Overlay.tsx
@@ -10,7 +10,7 @@ import { Body_3, Heading_3 } from "../../lib/fontStyle";
 
 const OverlaySection = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
 
   z-index: 1;
 
