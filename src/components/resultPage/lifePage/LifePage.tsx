6cc135990ae7cf30c986440b69359583e049a960
juhyojeong <juhyojeong58@gmail.com>
2022-08-02 14:52:07 +0900

    chore: 뇌증상 추가

--- src/components/resultPage/lifePage/LifePage.tsx
+++ src/components/resultPage/lifePage/LifePage.tsx
@@ -14,7 +14,11 @@ const TitleBox = styled.section`
 `;
 
 // 뇌증상
-const selectedType = ["62cd703fe49face142d9cffe"];
+const selectedType = [
+  "62e5283ae39eaf5b29f8adf3",
+  "62e5284be39eaf5b29f8adf4",
+  "62e5285de39eaf5b29f8adf5",
+];
 
 const LifePage = ({
   lifestyle,
