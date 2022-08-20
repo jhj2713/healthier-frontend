5d767eab576ef2cd6e6afd81d3d00b07b100d097
juhyojeong <juhyojeong58@gmail.com>
2022-08-21 00:17:16 +0900

    chore: 클래스명 수정

--- src/components/infoPage/Gender.tsx
+++ src/components/infoPage/Gender.tsx
@@ -13,7 +13,39 @@ const Title = styled(Body_4)`
 
   margin-bottom: 0.8rem;
 `;
-const GenderButton = styled(Body_2)<{
+const FemaleButton = styled(Body_2)<{
+  selected: boolean;
+}>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 4.8rem;
+  width: calc(100vw - 5.8rem);
+  font-weight: 200;
+
+  cursor: pointer;
+
+  & + & {
+    margin-left: 1rem;
+  }
+
+  ${({ selected }) =>
+    selected
+      ? css`
+          background-color: ${({ theme }) => theme.color.sub_blue};
+          color: ${({ theme }) => theme.color.blue};
+        `
+      : css`
+          background-color: transparent;
+          color: ${({ theme }) => theme.color.grey_600};
+          border: 0.1rem solid ${({ theme }) => theme.color.grey_600};
+        `}
+
+  border-radius: 0.8rem;
+  box-sizing: border-box;
+`;
+const MaleButton = styled(Body_2)<{
   selected: boolean;
 }>`
   display: flex;
@@ -51,12 +83,12 @@ const Gender = ({ gender, setGender }: IGenderProps) => {
     <Container>
       <Title>성별</Title>
       <GenderContainer>
-        <GenderButton onClick={() => setGender("m")} selected={gender === "m"}>
+        <MaleButton onClick={() => setGender("m")} selected={gender === "m"}>
           남성
-        </GenderButton>
-        <GenderButton onClick={() => setGender("f")} selected={gender === "f"}>
+        </MaleButton>
+        <FemaleButton onClick={() => setGender("f")} selected={gender === "f"}>
           여성
-        </GenderButton>
+        </FemaleButton>
       </GenderContainer>
     </Container>
   );
