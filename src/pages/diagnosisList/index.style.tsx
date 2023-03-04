7da93e98097932b86b471810b810779121663608
juhyojeong <juhyojeong58@gmail.com>
2023-03-04 12:02:52 +0900

    feat: 문진 결과 리스트 마크업

--- src/pages/diagnosisList/index.style.tsx
+++ src/pages/diagnosisList/index.style.tsx
@@ -1,5 +1,28 @@
+import { Heading_3, Description } from "src/lib/fontStyle";
 import styled from "styled-components";
 
-export const Container = styled.section`
-  padding-top: 5.6rem;
+export const Container = styled.div`
+  padding: 5.6rem 2.4rem;
+`;
+
+export const Title = styled(Heading_3)`
+  color: ${({ theme }) => theme.color.grey_200};
+
+  margin-top: 4rem;
+
+  .highlight {
+    color: ${({ theme }) => theme.color.green};
+  }
+`;
+
+export const DescriptionText = styled(Description)`
+  color: ${({ theme }) => theme.color.grey_200};
+
+  .highlight {
+    color: ${({ theme }) => theme.color.green};
+  }
+`;
+
+export const List = styled.div`
+  margin-top: 0.6rem;
 `;
