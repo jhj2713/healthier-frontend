08855b71bcbadc5e3c2d0592786b572939f5dc66
juhyojeong <juhyojeong58@gmail.com>
2023-03-12 16:08:11 +0900

    refactor: diagnosisCard navigate 과정 분리

--- src/pages/diagnosisList/index.tsx
+++ src/pages/diagnosisList/index.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { HeadacheDiagnose } from "src/api/diagnose";
 import DiagnosisCard from "src/components/diagnosisCard";
 import MainHeader from "src/components/mainHeader";
+import { IDiagnosisList } from "src/interfaces/component";
 import { IDiagnosisResultList } from "src/interfaces/diagnosticResult";
 import { Container, DescriptionText, List, Title } from "./index.style";
 
@@ -12,6 +14,17 @@ const DiagnosisList = () => {
   useEffect(() => {
     if (!state) navigate("/");
   }, []);
+
+  const handleNavigate = async (diag: IDiagnosisList) => {
+    const diagnosisResult = await HeadacheDiagnose.postResultDetail(Number(diag.record.diagnosis_id));
+
+    navigate("/result", {
+      state: {
+        type: "result",
+        diagnostic_result: diagnosisResult.diagnostic_result,
+      },
+    });
+  };
 
   return (
     <Container>
@@ -25,11 +38,21 @@ const DiagnosisList = () => {
       <DescriptionText style={{ marginTop: "2.4rem" }}>
         <span className="highlight">가장 가능성 높은 질환</span>
       </DescriptionText>
-      <List>{state && state.dataList.most_likely.map((diag, idx) => <DiagnosisCard key={idx} diagnosis={diag} />)}</List>
+      <List>
+        {state &&
+          state.dataList.most_likely.map((diag, idx) => (
+            <DiagnosisCard key={idx} diagnosis={diag} handleNavigate={() => handleNavigate(diag)} />
+          ))}
+      </List>
       <DescriptionText style={{ marginTop: "2.4rem" }}>
         <span className="highlight">다음 질환도 의심되어요!</span>
       </DescriptionText>
-      <List>{state && state.dataList.suspicious.map((diag, idx) => <DiagnosisCard key={idx} diagnosis={diag} />)}</List>
+      <List>
+        {state &&
+          state.dataList.suspicious.map((diag, idx) => (
+            <DiagnosisCard key={idx} diagnosis={diag} handleNavigate={() => handleNavigate(diag)} />
+          ))}
+      </List>
     </Container>
   );
 };
