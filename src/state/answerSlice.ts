61c149480e5ffa2ce68805982bc44905f72b7231
juhyojeong <juhyojeong58@gmail.com>
2023-05-31 14:10:20 +0900

    feat: 급성복통 로직 구현

---
+++ src/state/answerSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface AnswerState {
+  answers: { question_id: number; answer_id: number[] }[];
+}
+
+const initialState: AnswerState = {
+  answers: [],
+};
+
+export const answerSlice = createSlice({
+  name: "answer",
+  initialState,
+  reducers: {
+    popAnswer: (state) => {
+      state.answers.pop();
+    },
+    saveAnswer: (state, action: PayloadAction<{ question_id: number; answer_id: number[] }>) => {
+      state.answers = [...state.answers, action.payload];
+    },
+    resetAnswer: (state) => {
+      state.answers = [];
+    },
+  },
+});
+
+export const { popAnswer, saveAnswer, resetAnswer } = answerSlice.actions;
+
+export default answerSlice.reducer;
