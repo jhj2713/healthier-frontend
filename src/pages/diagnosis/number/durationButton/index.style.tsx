ae41bd72078dff2bdf387898f2e11d29f2390315
jin <53258214+kauthenticity@users.noreply.github.com>
2023-06-01 16:00:31 +0900

    feat: NUMBER_1, 2 버튼 생성

---
+++ src/pages/diagnosis/number/durationButton/index.style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const DurationButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DurationInput = styled.input``;
+
+export const DurationSelect = styled.select``;
+
+export const DurationText = styled.div`
+  color: #fff;
+`;
---
+++ src/pages/diagnosis/number/previousTimeButton/index.style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const PreviousTimeButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PreviousTimeInput = styled.input``;
+
+export const PreviousTimeSelect = styled.select``;
+
+export const PreviousTimeText = styled.div`
+  color: #fff;
+`;
