9101d39eda84df99a4d9050fc9f716fbc988c19a
jin <53258214+kauthenticity@users.noreply.github.com>
2023-06-04 14:33:05 +0900

    remove: 기존 증상 진단 커스텀훅 파일 삭제

--- src/api/diagnose/backpain.ts
+++
@@ -1,6 +0,0 @@
-import { diagnosisFetcher } from "./fetcher";
-import { IDiagnoseResponse } from "src/interfaces/diagnoseApi/diagnosis";
-
-export const BackpainDiagnose = {
-  getQuestions: (): Promise<IDiagnoseResponse> => diagnosisFetcher.get(`/backpain`),
-};
