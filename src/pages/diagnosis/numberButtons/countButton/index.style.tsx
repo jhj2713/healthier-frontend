7b9b5c3b6edca434766670819561badc2a28df74
jin <53258214+kauthenticity@users.noreply.github.com>
2023-06-15 19:31:49 +0900

    ✨ 증상 진단 횟수 버튼 UI 구현

--- src/pages/diagnosis/numberButtons/countButton/index.style.tsx
+++ src/pages/diagnosis/numberButtons/countButton/index.style.tsx
@@ -2,10 +2,36 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
 `;
 
-export const ButtonBox = styled.div`
-  color: ${({ theme }) => theme.color.white};
+export const Input = styled.input`
+  height: 5.2rem;
+  background: transparent;
+  text-align: center;
+  border: 1px solid ${({ theme }) => theme.color.grey_650};
+  border-radius: 3rem;
+  outline: none;
+  color: ${({ theme }) => theme.color.grey_300};
+  font-size: 1.6rem;
+  line-height: 1.5em;
+
+  &:focus {
+    border-color: #5464f2;
+  }
+
+  ::placeholder {
+    color: ${({ theme }) => theme.color.grey_600};
+    font-size: 1.6rem;
+    line-height: 1.5em;
+  }
+`;
+
+export const Text = styled.span`
+  font-size: 1.8rem;
+  line-height: 1.5em;
+  color: ${({ theme }) => theme.color.grey_300};
+  margin-left: 1.2rem;
+  font-weight: 200;
 `;
--- src/pages/diagnosis/numberButtons/countButton/index.tsx
+++ src/pages/diagnosis/numberButtons/countButton/index.tsx
@@ -1,39 +1,42 @@
-import { Dispatch, useEffect, useState, ChangeEvent } from "react";
+import { Dispatch, useState, ChangeEvent } from "react";
 import { IQuestion, IAnswer } from "src/interfaces/diagnoseApi/diagnosis";
 import { validateNumber } from "src/utils/inputValidator";
-import { ButtonBox, Container } from "./index.style";
+import * as Styled from "./index.style";
 
 interface ICountButtonProps {
   question: IQuestion;
   selectedAnswer: IAnswer[];
-  setSelectedAnswer: Dispatch<IAnswer[]>;
+  setSelectedAnswer: Dispatch<React.SetStateAction<IAnswer[]>>;
 }
 
 function CountButton({ setSelectedAnswer }: ICountButtonProps) {
   const [count, setCount] = useState<number>(0);
 
-  useEffect(() => {
-    setSelectedAnswer([
-      {
-        answer_id: 0,
-        answer: count + "번",
-        next_question: null,
-      },
-    ]);
-  }, [count, setSelectedAnswer]);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const number = validateNumber(e.target.value);
 
     setCount(number);
+
+    if (number === 0) {
+      setSelectedAnswer([]);
+
+      return;
+    }
+
+    setSelectedAnswer([
+      {
+        answer_id: 0,
+        answer: number + "번",
+        next_question: null,
+      },
+    ]);
   };
 
   return (
-    <Container>
-      <ButtonBox>
-        <input type="number" value={count} onChange={handleChange} />번
-      </ButtonBox>
-    </Container>
+    <Styled.Container>
+      <Styled.Input type="number" value={count || ""} placeholder="대략적인 횟수를 입력" onChange={handleInputChange} />
+      <Styled.Text>번</Styled.Text>
+    </Styled.Container>
   );
 }
 
