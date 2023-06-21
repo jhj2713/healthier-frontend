7cc324f8c76e893e6b07d89946265636a6d3c310
jin <53258214+kauthenticity@users.noreply.github.com>
2023-06-21 18:31:55 +0900

    ♻️ 기간 버튼 UI 수정

--- src/pages/diagnosis/numberButtons/durationButton/index.tsx
+++ src/pages/diagnosis/numberButtons/durationButton/index.tsx
@@ -1,11 +1,11 @@
 import { Dispatch, useState } from "react";
-import RoundButton from "src/components/roundButton";
+import TextFieldOutlined from "src/components/textFieldOutlined";
 import { IQuestion, IAnswer } from "src/interfaces/diagnoseApi/diagnosis";
 import theme from "src/lib/theme";
 import { validateNumber } from "src/utils/inputUtils";
 import * as Styled from "./index.style";
 
-const DURATION_TYPES = ["", "시간", "일", "주", "개월", "년"] as const;
+const DURATION_TYPES = ["시간", "일", "주", "개월", "년"] as const;
 
 type TDurationType = typeof DURATION_TYPES[number];
 
@@ -23,7 +23,7 @@ interface IDurationButtonProps {
 function DurationButton({ setSelectedAnswer }: IDurationButtonProps) {
   const [duration, setDuration] = useState<IDuration>({
     number: 0,
-    type: "",
+    type: "시간",
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,7 +32,7 @@ function DurationButton({ setSelectedAnswer }: IDurationButtonProps) {
 
     setDuration({ ...duration, number: number });
 
-    if (number === 0 || duration.type === "") {
+    if (number === 0) {
       setSelectedAnswer([]);
 
       return;
@@ -54,36 +54,25 @@ function DurationButton({ setSelectedAnswer }: IDurationButtonProps) {
   };
 
   return (
-    <>
+    <Styled.Container>
       <Styled.InputContainer>
-        <Styled.Input type="number" value={duration.number || ""} onChange={handleInputChange} placeholder="숫자 입력" />
-        {duration.type === "" ? (
-          <Styled.Input type="string" placeholder="버튼 선택" disabled />
-        ) : (
-          <Styled.Text color={theme.color.sub_blue} style={{ marginRight: "0.4rem" }}>
-            {duration.type}
-          </Styled.Text>
-        )}
+        <div style={{ width: "130px", marginRight: "1.2rem" }}>
+          <TextFieldOutlined type="number" value={duration.number || ""} onChange={handleInputChange} placeholder="숫자 입력" />
+        </div>
+        <Styled.Text color={theme.color.sub_blue} className="duration-type">
+          {duration.type}
+        </Styled.Text>
         <Styled.Text color={theme.color.grey_200}>전부터</Styled.Text>
       </Styled.InputContainer>
 
       <Styled.ButtonContainer>
-        {DURATION_TYPES.map((dt) =>
-          dt === "" ? null : (
-            <RoundButton
-              outline={duration.type === dt ? theme.color.sub_blue : theme.color.grey_650}
-              backgroundColor={duration.type === dt ? theme.color.sub_blue : "transparent"}
-              color={duration.type === dt ? "#5464F2" : theme.color.grey_300}
-              style={{ marginBottom: "1.2rem" }}
-              onClick={() => handleButtonClick(dt)}
-              key={dt}
-            >
-              {dt}
-            </RoundButton>
-          )
-        )}
+        {DURATION_TYPES.map((dt) => (
+          <Styled.DurationButton onClick={() => handleButtonClick(dt)} key={dt} selected={dt === duration.type}>
+            {dt}
+          </Styled.DurationButton>
+        ))}
       </Styled.ButtonContainer>
-    </>
+    </Styled.Container>
   );
 }
 
