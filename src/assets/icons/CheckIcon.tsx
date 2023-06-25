4a9f8db92c736323975b7a0563b79935fe458eb2
jin <53258214+kauthenticity@users.noreply.github.com>
2023-06-25 16:00:54 +0900

    🩹 icon 컴포넌트 attribute 오타 수정

--- src/assets/icons/CheckIcon.tsx
+++ src/assets/icons/CheckIcon.tsx
@@ -3,7 +3,7 @@ import { IIcon } from "src/interfaces/assets";
 export default function CheckIcon({ style = {}, width = 20, height = 20 }: IIcon) {
   return (
     <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" xmlns="http://www.w3.org/2000/svg" style={style}>
-      <path d="M18 5L7 16L2 11" stroke="#5464F2" stroke-width="2.4" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M18 5L7 16L2 11" stroke="#5464F2" strokeWidth="2.4" stroke-linecap="round" stroke-linejoin="round" />
     </svg>
   );
 }
