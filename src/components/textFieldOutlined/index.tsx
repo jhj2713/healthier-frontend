ba339fc881886c22e9751b329af263936097bec8
jin <53258214+kauthenticity@users.noreply.github.com>
2023-07-04 01:32:59 +0900

    🩹 TextFieldOutlined에 name props 추가

--- src/components/textFieldOutlined/index.tsx
+++ src/components/textFieldOutlined/index.tsx
@@ -9,10 +9,20 @@ interface ITextFieldOutlinedProps extends React.HTMLAttributes<HTMLInputElement>
   type?: TTextFieldOutlinedType;
   value: string | number;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name?: string;
 }
 
-function TextFieldOutlined({ borderRadius = "3rem", placeholder, type = "text", value, onChange }: ITextFieldOutlinedProps) {
-  return <Styled.Input type={type} style={{ borderRadius }} value={value} placeholder={placeholder} onChange={onChange}></Styled.Input>;
+function TextFieldOutlined({ borderRadius = "3rem", placeholder, type = "text", value, onChange, name }: ITextFieldOutlinedProps) {
+  return (
+    <Styled.Input
+      type={type}
+      style={{ borderRadius }}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      name={name}
+    ></Styled.Input>
+  );
 }
 
 export default TextFieldOutlined;
