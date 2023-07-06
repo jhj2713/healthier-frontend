a1f271e3e3f645cdaee31174413178e8e504844d
jin <53258214+kauthenticity@users.noreply.github.com>
2023-07-06 20:20:49 +0900

    🏷️ 이미지 버튼 부위 상수 생성

--- src/data/answer_type.ts
+++ src/data/answer_type.ts
@@ -15,3 +15,15 @@ export const ANSWER_TYPE = {
   DRAG_1: "DRAG_1",
   STR: "STR",
 } as const;
+
+export const DIGESTIVE_BODY_PART = {
+  "right-upper-stomach": "오른쪽 윗배",
+  "solar-plexus": "명치",
+  "left-upper-stomach": "왼쪽 윗배",
+  "right-side": "오른쪽 옆구리",
+  "belly-button": "배꼽 주위",
+  "left-side": "왼쪽 옆구리",
+  "right-lower-stomach": "오른쪽 아랫배",
+  "middle-lower-stomach": "아랫배 중앙",
+  "left-lower-stomach": "왼쪽 아랫배",
+} as const;
