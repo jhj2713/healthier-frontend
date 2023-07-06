b327d4d5cc76b33167c207286e220ac34c2eb3af
jin <53258214+kauthenticity@users.noreply.github.com>
2023-07-06 16:33:37 +0900

    🍱 소화기 통증 위치 아이콘 생성

---
+++ src/assets/bodyParts/DigestBottomLeft.tsx
@@ -0,0 +1,19 @@
+import { IIcon } from "src/interfaces/assets";
+
+export function DigestBottomLeft({ style }: IIcon) {
+  return (
+    <svg width="76" height="63" viewBox="0 0 76 63" fill="none" xmlns="http://www.w3.org/2000/svg" style={style}>
+      <g style={{ mixBlendMode: "multiply" }} opacity="0.8">
+        <path
+          d="M72 0.5H4.13539C1.22965 0.5 -0.410652 3.83612 1.36369 6.13721L27.8258 40.4552C28.154 40.8809 28.5758 41.2252 29.0585 41.4615L70.461 61.7315C72.7863 62.8699 75.5 61.1771 75.5 58.588V4C75.5 2.067 73.933 0.5 72 0.5Z"
+          fill="#6D7BF2"
+          fillOpacity="0.5"
+        />
+        <path
+          d="M72 0.5H4.13539C1.22965 0.5 -0.410652 3.83612 1.36369 6.13721L27.8258 40.4552C28.154 40.8809 28.5758 41.2252 29.0585 41.4615L70.461 61.7315C72.7863 62.8699 75.5 61.1771 75.5 58.588V4C75.5 2.067 73.933 0.5 72 0.5Z"
+          stroke="#5464F2"
+        />
+      </g>
+    </svg>
+  );
+}
---
+++ src/assets/bodyParts/DigestBottomRight.tsx
@@ -0,0 +1,19 @@
+import { IIcon } from "src/interfaces/assets";
+
+export function DigestBottomRight({ style }: IIcon) {
+  return (
+    <svg width="76" height="63" viewBox="0 0 76 63" fill="none" xmlns="http://www.w3.org/2000/svg" style={style}>
+      <g style={{ mixBlendMode: "multiply" }} opacity="0.8">
+        <path
+          d="M4 0.5H71.8646C74.7703 0.5 76.4107 3.83612 74.6363 6.13721L48.1742 40.4552C47.846 40.8809 47.4242 41.2252 46.9415 41.4615L5.539 61.7315C3.21367 62.8699 0.5 61.1771 0.5 58.588V4C0.5 2.067 2.067 0.5 4 0.5Z"
+          fill="#6D7BF2"
+          fillOpacity="0.5"
+        />
+        <path
+          d="M4 0.5H71.8646C74.7703 0.5 76.4107 3.83612 74.6363 6.13721L48.1742 40.4552C47.846 40.8809 47.4242 41.2252 46.9415 41.4615L5.539 61.7315C3.21367 62.8699 0.5 61.1771 0.5 58.588V4C0.5 2.067 2.067 0.5 4 0.5Z"
+          stroke="#5464F2"
+        />
+      </g>
+    </svg>
+  );
+}
