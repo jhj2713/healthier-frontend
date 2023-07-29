a68a9bd5916e165af467c49d055a2943ba40e8d4
jin <53258214+kauthenticity@users.noreply.github.com>
2023-07-29 18:39:20 +0900

    🩹 svg 속성 오류 수정

--- src/assets/icons/ChevronLeftIcon.tsx
+++ src/assets/icons/ChevronLeftIcon.tsx
@@ -3,7 +3,7 @@ import { IIcon } from "src/interfaces/assets";
 export default function ChevronLeftIcon({ width = 32, height = 32, stroke = "#FDFDFD" }: IIcon) {
   return (
     <svg width={width} height={height} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M20 24L12 16L20 8" stroke={stroke} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M20 24L12 16L20 8" stroke={stroke} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 }
