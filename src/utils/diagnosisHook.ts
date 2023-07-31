83ad8278fb81721223f1270e0d1384e51f6452bc
jin <53258214+kauthenticity@users.noreply.github.com>
2023-07-31 19:31:41 +0900

    🐛 생년월일 형식 버그 수정

--- src/utils/diagnosisHook.ts
+++ src/utils/diagnosisHook.ts
@@ -17,8 +17,8 @@ export const getNextQuestion = ({ selectedAnswer, curQuestion, questions }: IGet
 };
 
 export const formatBirth = ({ year, month, date }: { year: number; month: number; date: number }): string => {
-  const formattedMonth = (month || 1) < 10 ? "0" + month : month;
-  const formattedDate = (date || 1) < 10 ? "0" + date : date;
+  const formattedMonth = month < 10 ? "0" + (month || 1) : month;
+  const formattedDate = date < 10 ? "0" + (date || 1) : date;
 
   return `${year}-${formattedMonth}-${formattedDate}`;
 };
