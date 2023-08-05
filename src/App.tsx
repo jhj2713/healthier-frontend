c4574f6cf8a5b3ca0c664903ff57d15a8dc8d826
juhyojeong <juhyojeong58@gmail.com>
2023-08-05 21:09:45 +0900

    fix: 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,6 @@ import {
   Appointment,
   Error,
   Test,
-  InformationQR,
-  SymptomTypeQR,
-  CompleteQR,
 } from "./pages";
 import { useAppSelector } from "./state";
 
@@ -58,9 +55,6 @@ function App() {
             <Route path="/symptom-type" element={<SymptomTypePage />} />
             <Route path="/appointment" element={<Appointment />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/qr/info" element={<InformationQR />} />
-            <Route path="/qr/symptom-type" element={<SymptomTypeQR />} />
-            <Route path="/qr/complete" element={<CompleteQR />} />
             <Route path="/test" element={<Test />} />
             <Route path="/*" element={<Navigate to="/" replace />} />
           </Routes>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import Appointment from "./appointment";
-import CompleteQR from "./complete-qr";
 import Diagnosis from "./diagnosis";
 import DiagnosisList from "./diagnosisList";
 import Error from "./error";
-import InformationQR from "./info-qr";
 import Information from "./information";
 import MainPage from "./main";
 import MyDiagnosis from "./myDiagnosis";
 import ResultPage from "./result";
 import SignUp from "./signUp";
 import SymptomPage from "./symptom";
-import SymptomTypeQR from "./symptom-qr";
 import SymptomTypePage from "./symptomType";
 import Test from "./test";
 
@@ -27,7 +24,4 @@ export {
   Error,
   Test,
   Appointment,
-  InformationQR,
-  SymptomTypeQR,
-  CompleteQR,
 };
